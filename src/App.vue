cc76d10125f8747d95d4271e4f9cf7ba31dbb06e
liangjiayy <liangjiayy@qq.com>
2025-03-02 20:23:44 +0800

    删除无用代码

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,8 @@
-<!-- eslint-disable @typescript-eslint/no-unused-vars -->
 <script setup lang="ts">
-// import { RouterLink, RouterView } from 'vue-router'
-// import HelloWorld from './components/HelloWorld.vue'
-import QuestionImport from './views/question/QuestionImport.vue';
-import QuestionMain from './views/question/QuestionMain.vue'
 </script>
 
 <template>
   <header>
-    <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/import">导入题库</RouterLink>
-        <RouterLink to="/main">测试</RouterLink>
-      </nav>
-    </div> -->
     <nav>
       <RouterLink to="/">Home</RouterLink>
       <RouterLink to="/about">About</RouterLink>
